b19180ea164c62ec99c3ffa4e71c311a097d05c7
htwright <harrisontwright@gmail.com>
2017-04-05 10:14:21 -0500

    first drill

---
+++ object-drills1.js
@@ -0,0 +1,13 @@
+function createMyObject() {
+	const myObject = {
+		foo:'bar',
+		answerToUniverse:42,
+		"olly olly":'oxen free',
+		sayHello:function(){
+			return 'hello';
+		}
+
+	}
+	console.log(myObject);
+}
+createMyObject();
